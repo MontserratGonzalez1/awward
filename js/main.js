54b6bf8ff04c45d2eb63dcc9d5730af4e1f04e3b
Montserratgonzalez1 <monkygs@gmail.com>
2016-12-15 17:21:54 -0600

    estructura a la mitad, no puedo hacer hover

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/* menu */
+$(".button-collapse").sideNav();
+
+
+  $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
+        
+// Hide sideNav
+  $('.button-collapse').sideNav('hide');
+
