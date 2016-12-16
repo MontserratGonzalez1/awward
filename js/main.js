e90c01e98d66936f967a07240775f704587d5462
Montserratgonzalez1 <monkygs@gmail.com>
2016-12-15 20:04:20 -0600

    footer y nav ya se mueven. aun nohay hovers y demás efectos

--- js/main.js
+++ js/main.js
@@ -13,3 +13,16 @@ $(".button-collapse").sideNav();
 // Hide sideNav
   $('.button-collapse').sideNav('hide');
 
+/*footer*/
+$(document).ready(function(){
+	var altura = $('.menu').offset().top;
+	
+	$(window).on('scroll', function(){
+		if ( $(window).scrollTop() > altura ){
+			$('.menu').addClass('menu-fixed');
+		} else {
+			$('.menu').removeClass('menu-fixed');
+		}
+	});
+ 
+});
